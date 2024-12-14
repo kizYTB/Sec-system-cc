804e73d219765cee73288ae4c9cf20672a5f31b0
kiz___ <140178579+kizYTB@users.noreply.github.com>
2024-12-14 19:55:00 +0100

    Update install.lua

--- install.lua
+++ install.lua
@@ -9,6 +9,20 @@ else
     install()
 end
 
+local function install()
+    run("cd","/")
+    run("wget","https://raw.githubusercontent.com/kizYTB/Sec-system-cc/refs/heads/main/startup.lua")
+    run("wget","https://raw.githubusercontent.com/kizYTB/Sec-system-cc/refs/heads/main/.settings")
+    run("mkdir",".sec")
+    run("cd","/.sec")
+    run("wget","https://raw.githubusercontent.com/kizYTB/Sec-system-cc/refs/heads/main/.sec/resetpass.lua")
+    run("wget","https://raw.githubusercontent.com/kizYTB/Sec-system-cc/refs/heads/main/.sec/pass.lua")
+    run("wget","https://raw.githubusercontent.com/kizYTB/Sec-system-cc/refs/heads/main/.sec/bootmanager.lua")
+    run("mkdir","pass/")
+    run("cd","/.sec/pass/")
+    run("wget","https://raw.githubusercontent.com/kizYTB/Sec-system-cc/refs/heads/main/.sec/pass/data.txt")
+end
+
 local function DELETESTART()
 
 textutils.slowPrint("All operating system files, Bootloader will be deleted, you have 10 seconds to stop via the button on the top left of the screen.", 15)
@@ -23,18 +37,4 @@ sleep(2)
 
 install()
 
-end
-
-local function install()
-    run("cd","/")
-    run("wget","https://raw.githubusercontent.com/kizYTB/Sec-system-cc/refs/heads/main/startup.lua")
-    run("wget","https://raw.githubusercontent.com/kizYTB/Sec-system-cc/refs/heads/main/.settings")
-    run("mkdir",".sec")
-    run("cd","/.sec")
-    run("wget","https://raw.githubusercontent.com/kizYTB/Sec-system-cc/refs/heads/main/.sec/resetpass.lua")
-    run("wget","https://raw.githubusercontent.com/kizYTB/Sec-system-cc/refs/heads/main/.sec/pass.lua")
-    run("wget","https://raw.githubusercontent.com/kizYTB/Sec-system-cc/refs/heads/main/.sec/bootmanager.lua")
-    run("mkdir","pass/")
-    run("cd","/.sec/pass/")
-    run("wget","https://raw.githubusercontent.com/kizYTB/Sec-system-cc/refs/heads/main/.sec/pass/data.txt")
 end

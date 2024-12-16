a7cd44e6b8af295c3fcb5c00c25c9b1ae6e2c1bc
kiz___ <140178579+kizYTB@users.noreply.github.com>
2024-12-16 16:44:43 +0100

    .

--- install.lua
+++ install.lua
@@ -3,6 +3,22 @@ local run = shell.run
 term.clear()
 term.setCursorPos(1,1)
 
+local function installation()
+    run("cd","/")
+    run("wget","https://raw.githubusercontent.com/kizYTB/Sec-system-cc/refs/heads/main/startup.lua")
+    run("mkdir",".sec")
+    run("cd","/.sec")
+    run("wget","https://raw.githubusercontent.com/kizYTB/Sec-system-cc/refs/heads/main/.sec/resetpass.lua")
+    run("wget","https://raw.githubusercontent.com/kizYTB/Sec-system-cc/refs/heads/main/.sec/pass.lua")
+    run("wget","https://raw.githubusercontent.com/kizYTB/Sec-system-cc/refs/heads/main/.sec/bootmanager.lua")
+    run("mkdir","pass/")
+    run("cd","/.sec/pass/")
+    run("wget","https://raw.githubusercontent.com/kizYTB/Sec-system-cc/refs/heads/main/.sec/pass/data.txt")
+
+    os.reboot()
+end
+
+
 local function DELETESTART()
 
     textutils.slowPrint("All operating system files, Bootloader will be deleted, you have 10 seconds to stop via the button on the top left of the screen.", 15)
@@ -19,21 +35,6 @@ local function DELETESTART()
     
 end
 
-local function installation()
-    run("cd","/")
-    run("wget","https://raw.githubusercontent.com/kizYTB/Sec-system-cc/refs/heads/main/startup.lua")
-    run("mkdir",".sec")
-    run("cd","/.sec")
-    run("wget","https://raw.githubusercontent.com/kizYTB/Sec-system-cc/refs/heads/main/.sec/resetpass.lua")
-    run("wget","https://raw.githubusercontent.com/kizYTB/Sec-system-cc/refs/heads/main/.sec/pass.lua")
-    run("wget","https://raw.githubusercontent.com/kizYTB/Sec-system-cc/refs/heads/main/.sec/bootmanager.lua")
-    run("mkdir","pass/")
-    run("cd","/.sec/pass/")
-    run("wget","https://raw.githubusercontent.com/kizYTB/Sec-system-cc/refs/heads/main/.sec/pass/data.txt")
-
-    os.reboot()
-end
-
 if fs.exists("/startup.lua") then
     textutils.slowPrint("All operating system files, Bootloader will be deleted, you have 10 seconds to stop via the button on the top left of the screen.", 15)
 
@@ -45,7 +46,7 @@ if fs.exists("/startup.lua") then
 
     sleep(2)
 
-    install()
+    installation()
 
 else
     installation()

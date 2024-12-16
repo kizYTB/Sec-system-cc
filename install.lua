bacbe5ea9972be6842fa62137620d7ec1d5d1077
kiz___ <140178579+kizYTB@users.noreply.github.com>
2024-12-16 16:39:53 +0100

    .

--- install.lua
+++ install.lua
@@ -4,24 +4,23 @@ term.clear()
 term.setCursorPos(1,1)
 
 if fs.exists("/startup.lua") then
-    DELETESTART()
+    textutils.slowPrint("All operating system files, Bootloader will be deleted, you have 10 seconds to stop via the button on the top left of the screen.", 15)
+
+    sleep(10)
+
+    textutils.slowPrint("Deleting boot files from the os, bootloader.",15)
+
+    run("rm","/startup.lua")
+
+    sleep(2)
+
+    install()
+
 else
     install()
 end
 
 local function DELETESTART()
-
-textutils.slowPrint("All operating system files, Bootloader will be deleted, you have 10 seconds to stop via the button on the top left of the screen.", 15)
-
-sleep(10)
-
-textutils.slowPrint("Deleting boot files from the os, bootloader.",15)
-
-run("rm","/startup.lua")
-
-sleep(2)
-
-install()
 
 end
 
